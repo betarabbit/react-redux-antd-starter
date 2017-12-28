286cf78f8c34fe067ca9981163e118d128b7f8db
Francisco Souza <francisco.souza@ifnet-for.ifactory.com.br>
2017-12-28 17:14:32 -0300

    Added Boilerplate
    - Added tracker for the redux store
    - Added reducers and main component support
    - created root reducer
    - added some useful packages to deal with forms, promises and the router-dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import './styles/base.less';
 
-import App from './containers/App';
 import React from 'react';
 import ReactDom from 'react-dom';
+import promise from 'redux-promise';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './containers/App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const reduxStore = createStoreWithMiddleware(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDom.render(
-  <App />, document.getElementById('root')
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root')
 );
